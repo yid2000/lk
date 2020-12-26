aadc23da7d7d620e919b19296509560a5e76e7b0
AA <AA@aa.com>
2020-12-26 12:09:16 -0800

    java, hello

---
+++ hello.java
@@ -0,0 +1,11 @@
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+public class hello
+{
+  public static void main(String[] args) {
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    LocalDateTime now = LocalDateTime.now();
+    System.out.println("Hi, current time: " + dtf.format(now));
+  }
+}
