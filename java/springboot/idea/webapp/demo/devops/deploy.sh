9a49348030c2e84955efd1bfac3c56550a6da08f
yid2000 <71228913+yid2000@users.noreply.github.com>
2021-01-03 13:19:33 -0800

    deploy.sh: dynamic shared sub dir name

--- java/springboot/idea/webapp/demo/devops/deploy.sh
+++ java/springboot/idea/webapp/demo/devops/deploy.sh
@@ -1,2 +1,2 @@
-mkdir -p /home/jenkins/share/$GIT_BRANCH/$BUILD_NUMBER
-cp java/springboot/idea/webapp/demo/target/demo-0.0.1-SNAPSHOT.jar /home/jenkins/share/$GIT_BRANCH/$BUILD_NUMBER/demo.jar
+mkdir -p /home/jenkins/share/$GIT_LOCAL_BRANCH/$BUILD_NUMBER
+cp java/springboot/idea/webapp/demo/target/demo-0.0.1-SNAPSHOT.jar /home/jenkins/share/$GIT_LOCAL_BRANCH/$BUILD_NUMBER/demo.jar
