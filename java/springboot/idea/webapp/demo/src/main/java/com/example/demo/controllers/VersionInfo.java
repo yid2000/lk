a55e36c2c113a2625286517257da647827180c24
yid2000 <71228913+yid2000@users.noreply.github.com>
2021-01-03 17:26:46 -0800

    env specific properties

--- java/springboot/idea/webapp/demo/src/main/java/com/example/demo/controllers/VersionInfo.java
+++ java/springboot/idea/webapp/demo/src/main/java/com/example/demo/controllers/VersionInfo.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="/api", produces = "application/json")
 public class VersionInfo {
+
+    @Value("${app.ver}")
+    private String ver;
     @GetMapping("/ver")
     public String getVersion() {
-        return "0.2";
+        return ver;
     }
 }
