db5dacbdb11d6423c3fd97a8637347e7e9119c33
yid2000 <71228913+yid2000@users.noreply.github.com>
2021-01-27 09:37:06 -0800

    nagios

--- nagios/CustomPlugins/port_check_nc.bat
+++ nagios/CustomPlugins/port_check_nc.bat
@@ -1,16 +1,28 @@
 set server=%1
 set port=%2
-set os=%3
-set source_server=%4
 
-set	nc_cmd_path=
-if /i "%os:~0,3%"=="win" set nc_cmd_path=ExternalTools\nc.exe
-if /i "%os:~0,3%" neq "win" set nc_cmd_path=nc
+set	nc_cmd=..\ExternalTools\nc.exe
+set nc_cmd=%nc_cmd% -i 4 -w 4 -vvv %server% %port%
+set result=
+set exitcode=3
+set timestamp=%date:~10,4%%date:~4,2%%date:~7,2%_%time:~0,8%
+set timestamp=%timestamp::=_%
+set outfilename=%server%_%port%_%timestamp%.out
+set is_success=false
+%nc_cmd% > %outfilename% 2>&1
 
-set nc_cmd=%nc_cmd_path% -i 4 -w 4 -vvv %server% %port%
-set out_file_name=%source_server%_%server%_%port%.txt
-%nc_cmd% 2>&1>%out_file_name%
-exit /b 0
+findstr /c:"CONNECT SUCCESS" %outfilename% > NUL
+if %errorlevel% EQU 0 ( set is_success=true)
+
+if "%is_success%"=="true" (
+	set result=CONNECTION SUCCESSFUL
+	set exitcode=0
+) else (
+	set result=CONNECTION FAILED
+	set exitcode=3
+)
+echo %result%:exitcode=%exitcode%:%nc_cmd%
+exit /b %exitcode%
 
 
 
